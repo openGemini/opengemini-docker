0f279db749512a0843c14b74fc97977bd4bce5ed
shilinlee <836160610@qq.com>
2023-09-25 20:01:52 +0800

    feat: support opengemini server images (#1)
    Signed-off-by: shilinlee <836160610@qq.com>

---
+++ opengemini-server/1.1.0-rc0/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# dynamic configuration parameters by --config=item
+if [ "${1:0:1}" = '-' ]; then
+    set -- ts-server "$@"
+fi
+
+# execute `ts-server CMD`
+if [ "$1" = 'ts-server' ]; then
+	/init-server.sh "${@:2}"
+fi
+
+exec "$@" -config /etc/openGemini/server.conf
